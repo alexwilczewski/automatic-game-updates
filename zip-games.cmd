6c974509a1029eed715ea973cb6174c93d993c73
Alexander Wilczewski <alex@wilczewski.org>
2018-08-03 07:44:11 -0700

    Breaking changes. + Updated zipping conf structure. + More fine grain control of output zip. + Performance improvement using 7z update file compare.

--- zip-games.cmd
+++ zip-games.cmd
@@ -2,32 +2,32 @@
 SET cwd=%~dp0
 SET readin=%1
 SET sevenzip="%cwd%7za.exe"
-SET storezipprefix=">"
-SET addprefix="<"
-SET storezip=
-FOR /F "usebackq eol=; tokens=1,2,3* delims=|" %%a IN (%readin%) DO (
-  IF "%%a"==%storezipprefix% (
-    CALL :givenstore "%%b"
-	CALL :resetstore
-  )
-  IF "%%a"==%addprefix% (
-    CALL :givenaddition "%%b" "%%c"
-  )
+SET filesonlyflag=F
+FOR /F "usebackq eol=; tokens=1,2,3 delims=|" %%a IN (%readin%) DO (
+  CALL :givenline "%%a" "%%b" %%c
 )
 EXIT /B 0
 
-:givenstore
-SET storezip=%1
-EXIT /B 0
-
-:resetstore
-IF EXIST %storezip% (
-  CALL del %storezip%
+:givenline
+SET type=%3
+IF %type%==%filesonlyflag% (
+  CALL :zipnonrecursive %1 %2
+) ELSE (
+  CALL :ziprecursive %1 %2
 )
 EXIT /B 0
 
-:givenaddition
-SET systemfile=%1
-SET innertarfile=%2
-CALL %sevenzip% a dummy -ttar -so %systemfile% | %sevenzip% a -t7z -si%innertarfile% %storezip%
+:zipnonrecursive
+SET systemlocation=%~1
+SET output=%2
+SET dircall="dir ""%systemlocation%"" /AA /B"
+FOR /F %%a IN ('%dircall%') DO (
+  CALL :ziprecursive "%systemlocation%\%%a" %output%
+)
+EXIT /B 0
+
+:ziprecursive
+SET systemlocation=%1
+SET output=%2
+CALL %sevenzip% a -y -ur2y2 %output% %systemlocation%
 EXIT /B 0
