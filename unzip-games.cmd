efd6485d2dcd07258d7e20d16e0a2637f5542a75
Alexander Wilczewski <alex@wilczewski.org>
2018-08-03 20:12:55 -0700

    BREAKING CHANGE Client now reads in zip files, removed tar handling.

--- unzip-games.cmd
+++ unzip-games.cmd
@@ -2,35 +2,16 @@
 SET cwd=%~dp0
 SET readin=%1
 SET sevenzip="%cwd%7za.exe"
-SET ziptmpfile="%temp%\TempZippedGames.7z"
-SET usezipprefix="<"
-SET pullprefix=">"
-FOR /F "usebackq eol=; tokens=1,2,3* delims=|" %%a IN (%readin%) DO (
-  IF "%%a"==%usezipprefix% (
-    CALL :givenusezip "%%b"
-  )
-  IF "%%a"==%pullprefix% (
-    CALL :givenpull "%%b" "%%c"
-  )
+SET ziptmpfile="%temp%\tempgame.7z"
+FOR /F "usebackq eol=; tokens=1,2 delims=|" %%a IN (%readin%) DO (
+  CALL :givenline "%%a" "%%b"
 )
-CALL :cleanupzip
 EXIT /B 0
 
-:givenusezip
-CALL :downloadzip %1
-EXIT /B 0
-
-:downloadzip
+:givenline
 SET zipurl=%1
+SET unzipdir=%2
 CALL "%cwd%curl.exe" -o %ziptmpfile% %zipurl%
-EXIT /B 0
-
-:givenpull
-SET from=%1
-SET to=%2
-CALL %sevenzip% e %ziptmpfile% %from% -so | %sevenzip% x -y -si -ttar -o%to%
-EXIT /B 0
-
-:cleanupzip
+CALL %sevenzip% x %ziptmpfile% -y -o%unzipdir% -r
 CALL DEL %ziptmpfile%
 EXIT /B 0
